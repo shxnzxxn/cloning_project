88776d13cc5d4a712794d1ce803e7ec340507104
skj4244 <skj4244@naver.com>
2023-10-06 08:35:54 +0900

    feat: Menu

--- src/main/java/cloning_project/demo/Menu/Menu.java
+++ src/main/java/cloning_project/demo/Menu/Menu.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
